c9403ab55067b8992b24622a6f8c9b24d5df82e6
KyotoLee <kyotolee69@gmail.com>
2023-03-27 16:35:47 +0700

    create table customer, store, staff, admin

---
+++ database/migrations/2023_03_27_092638_create_staffs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('password');
+            $table->string('email');
+            $table->tinyInteger('role')->default(2)->comment('1: Manager, 2: staff');
+            $table->tinyInteger('status')->default(1);
+            $table->string('phone');
+            $table->string('address');
+            $table->string('avatar');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+};
